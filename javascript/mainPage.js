a6196a773cb01bf6c97dd50f20f55472b67a2b4b
Ruisong4 <ruisong4@illinois.edu>
2019-04-05 00:45:15 -0500

    finish seaerch box ui

--- javascript/mainPage.js
+++ javascript/mainPage.js
@@ -1,13 +1,47 @@
 $('document').ready(function () {
 
-    $('#depDateInput').flatpickr();
-    $('#desDateInput').flatpickr();
+    let departureDate = flatpickr('#depDateInput', {});
+    let returnDate = flatpickr('#desDateInput', {});
+    let numberDate = flatpickr('#flightNumberDate', {});
+    let routeSuggestDep;
+    let routeSuggestDes;
+    let flightSuggestDep;
+    let flightSuggestDes;
 
-    let xhr;
+    $("#byRoute").click(function () {
+        $(".routeBlock").css("display","block");
+        $(".flightBlock").css("display","none");
+        $("#byRoute").css({
+            "color":"#dfe4eb",
+            "background":"#eba718",
+            "cursor":"default"
+        });
+        $("#byFlight").css({
+            "color":"#000",
+            "background":"#dfe4eb",
+            "cursor":"pointer"
+        });
+    });
+
+    $("#byFlight").click(function () {
+        $(".flightBlock").css("display","block");
+        $(".routeBlock").css("display","none");
+        $("#byFlight").css({
+            "color":"#dfe4eb",
+            "background":"#eba718",
+            "cursor":"default"
+        });
+        $("#byRoute").css({
+            "color":"#000",
+            "background":"#dfe4eb",
+            "cursor":"pointer"
+        });
+    });
+
     $('#depInput').autoComplete({
         source: function(term, response){
-            try { xhr.abort(); } catch(e){}
-            xhr = $.getJSON('./php/suggestion.php', { q: term }, function(data){ response(data); });
+            try { routeSuggestDep.abort(); } catch(e){}
+            routeSuggestDep = $.getJSON('./php/suggestion.php', { q: term}, function(data){ response(data); });
         }
     });
 });
