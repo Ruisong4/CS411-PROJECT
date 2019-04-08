9162e5ab2d925f111f8cb8dcb4fc6d4799f98675
jiayuan8 <jiayuan8@illinois.edu>
2019-04-07 19:34:17 -0500

    fix connect

--- php/connect.php
+++ php/connect.php
@@ -1,8 +1,8 @@
 <?php
     $server = "localhost";
-    $db_username = "root"; //helloworldflight_jiayuan8
-    $db_password = ""; //a321a321a330a340
-    $dbname = "helloworldflight_Flight";
+    $db_username = "root"; 
+    $db_password = "cs411fullscore"; 
+    $dbname = "flightdb";
     $conn = new mysqli($server, $db_username, $db_password, $dbname);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
