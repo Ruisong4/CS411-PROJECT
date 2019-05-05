e1f539c09645665a556e694fb6a468b5ff66eb1a
Ruisong4 <ruisong4@illinois.edu>
2019-05-04 22:02:14 -0500

    finish user review

--- javascript/tripHistory.js
+++ javascript/tripHistory.js
@@ -13,20 +13,20 @@ $('document').ready(function () {
             success: function (data, textStatus, jqXHR) {
                 for (let i = 0; i < data.length; i++){
                     let a = $( "<div class=\'reviewItem\'></div>" );
-                    let b = $( "<div></div>" ).text(data[0].airline);
-                    let c = $( "<div></div>" ).text(data[0].flight_number);
-                    let d = $( "<div></div>" ).text(data[0].flight_date);
+                    let b = $( "<div></div>" ).text(data[i].airline);
+                    let c = $( "<div></div>" ).text(data[i].flight_number);
+                    let d = $( "<div></div>" ).text(data[i].flight_date);
                     let e = $( "<div></div>" );
-                    if (data[0].no_delay){
+                    if (data[i].no_delay == "1"){
                         e.text("No Delay")
                     }
-                    if (data[0].below_fifteen){
+                    if (data[i].below_fifteen == "1"){
                         e.text("Around Thirty Minutes")
                     }
-                    if (data[0].below_thirty){
+                    if (data[i].below_thirty == "1"){
                         e.text("Around Thirty Minutes")
                     }
-                    if (data[0].above_thirty){
+                    if (data[i].above_thirty == "1"){
                         e.text("Above Thirty Minutes")
                     }
                     a.append(b,c,d,e);
@@ -53,7 +53,6 @@ $('document').ready(function () {
     $('#insertSubmit').on('click',function () {
         let airline = $('#insertAirline').val();
         let flight =$('#insertNumber').val();
-        alert(dateInput.selectedDates);
         let dateInput2 = dateInput.selectedDates == "" ? "" : new Date(dateInput.selectedDates);
         let month = dateInput2.getMonth() + 1; //months from 1-12
         let day = dateInput2.getDate();
@@ -71,14 +70,50 @@ $('document').ready(function () {
             dataType: "json",
             success: function (data, textStatus, jqXHR) {
                 if (data.status == true) {
+                    let year = new Date();
+                    year = year.getFullYear();
+                    let index = document.getElementById("insertInfo").selectedIndex;
+                    let s1 = index == "0" ? 1 : 0;
+                    let s2 = index == "1" ? 1 : 0;
+                    let s3 = index == "2" ? 1 : 0;
+                    let s4 = index == "3" ? 1 : 0;
+                    $.ajax({
+                        type: "POST",
+                        url: "./php/insertReview.php",
+                        data: {
+                            "airline": airline,
+                            "flight": flight,
+                            "date":year + "-" + month + "-" + day,
+                            "s1":s1,
+                            "s2":s2,
+                            "s3":s3,
+                            "s4":s4,
+                            "user":getCookie("username")
+                        },
+                        cache: false,
+                        async: false,
+                        dataType: "json",
+                        success: function (data, textStatus, jqXHR) {
+                            if (data.status == true) {
+                                alert("your feedback is important for us, thank you!");
+                                location.reload();
+                            } else {
+                                alert("You can not comment on one flight twice");
+                            }
+                        },
+                        error: function (XMLHttpRequest, textStatus, errorThrown) {
 
+                        }
+                    });
                 } else {
                     alert("there is no such flight at the given date")
+                    return;
                 }
             },
             error: function (XMLHttpRequest, textStatus, errorThrown) {
 
             }
         });
+
     })
 });
--- php/insertReview.php
+++ php/insertReview.php
@@ -1,1 +1,29 @@
 <?php
+    include('connect.php');
+    $airline = $_POST["airline"];
+    $flight = $_POST["flight"];
+    $date = $_POST["date"];
+    $user = $_POST["user"];
+    $s1 = $_POST["s1"];
+    $s2 = $_POST["s2"];
+    $s3 = $_POST["s3"];
+    $s4 = $_POST["s4"];
+
+    $insert = "INSERT INTO `user_review`(`flight_date`, `airline`, `flight_number`, `no_delay`, 
+                    `below_fifteen`, `below_thirty`, `above_thirty`, `user`) 
+                    VALUES ('$date','$airline','$flight','$s1','$s2'
+                    ,'$s3','$s4','$user')";
+
+    if(!$conn->query($insert)){
+        $response = [
+            'status' => false,
+            'message' => 'na'
+        ];
+        echo json_encode($response);
+    }else{
+        $response = [
+            'status' => true,
+            'message' => 'na'
+        ];
+        echo json_encode($response);
+    };
